68193cefb0bfd450f0264c33c542ea22a37fec50
irumvabric <irumvabric#@gmail.com>
2024-09-26 20:35:47 +0200

    Now ni bon ordi:ok phone:ok

--- lib/main.dart
+++ lib/main.dart
@@ -37,41 +37,15 @@ class _SplashScreenState extends State<SplashScreen> {
     Future.delayed(const Duration(seconds: 1), () {
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(
-            builder: (context) =>
-                const HomePage()),
+        MaterialPageRoute(builder: (context) => const HomePage()),
       );
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/OXO.png',
-              height: 150,
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Welcome to Tic-Tac-Toe',
-              style: TextStyle(
-                fontSize: 24,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 20),
-            const CircularProgressIndicator(
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-            ), 
-          ],
-        ),
-      ),
+    return MaterialApp(
+      home: const HomePage(),
     );
   }
 }
